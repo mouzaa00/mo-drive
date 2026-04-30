cfe7cef83252fd5042634de49e4979be6e88c6ff
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2026-04-30 02:02:37 +0100

    fix signing out

--- src/components/sign-out.tsx
+++ src/components/sign-out.tsx
@@ -2,10 +2,22 @@
 
 import { authClient } from "~/lib/auth-client";
 import { Button } from "./ui/button";
+import { redirect } from "next/navigation";
 
 export function SignOut() {
   return (
-    <Button variant="outline" onClick={async () => await authClient.signOut()}>
+    <Button
+      variant="outline"
+      onClick={async () =>
+        await authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              redirect("/login");
+            },
+          },
+        })
+      }
+    >
       sign out
     </Button>
   );
