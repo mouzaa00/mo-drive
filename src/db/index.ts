f81529354a5a1af6df32bea51fe96ce7a3bb717a
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2026-04-30 01:59:08 +0100

    maintain a single db connection during development

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,15 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "@neondatabase/serverless";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
+const globalForDb = globalThis as unknown as {
+  pool: Pool | undefined;
+};
+
+const pool =
+  globalForDb.pool ??
+  new Pool({
+    connectionString: process.env.DATABASE_URL!,
+  });
+if (process.env.NODE_ENV !== "production") globalForDb.pool = pool;
 
 export const db = drizzle(pool);
